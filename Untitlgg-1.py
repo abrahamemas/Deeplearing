d603a9cb240c364406f8781326ed6a3adbf5572d
abrahamemas <abrahamemas@gmail.com>
2022-06-10 10:19:14 +0100

    files

---
+++ Untitlgg-1.py
@@ -0,0 +1,38 @@
+bag = 50
+shoe = 150
+glass = 200
+phone = 150
+
+if bag <= glass :
+    print('Yes its Ture ')
+elif shoe >= bag :
+    print('that is correct')
+elif shoe == phone :
+    print('You are right')
+elif glass <= bag :
+    print('You are wrong')
+else :
+    print('not working')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
